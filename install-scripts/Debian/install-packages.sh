492ac84187c6bcb0847905d041f6dbb783028eba
JinYan <yan324@gmail.com>
2021-12-10 15:25:04 +0800

    update

--- install-scripts/Debian/install-packages.sh
+++ install-scripts/Debian/install-packages.sh
@@ -12,7 +12,7 @@ else
     echo "zsh FAILED TO INSTALL!!!" >> $log_file
 fi
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh --skip-chsh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --skip-chsh"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
