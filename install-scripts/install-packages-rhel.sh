c28e00798f6bce9e0e79656a55e7986dc8cca1ad
JinYan <yan324@gmail.com>
2023-01-12 15:11:43 +0800

    update

--- install-scripts/install-packages-rhel.sh
+++ install-scripts/install-packages-rhel.sh
@@ -11,7 +11,7 @@ else
     echo "zsh FAILED TO INSTALL!!!" >> $log_file
 fi
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --skip-chsh"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${HOME}/.oh-my-zsh/custom/plugins/zsh-completions
